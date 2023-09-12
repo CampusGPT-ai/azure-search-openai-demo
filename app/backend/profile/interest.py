21afa2ab4b02d0567e2ae1a379e2587eace583f9
Reinhold Staudinger <rstaudinger@reinholds-mbp.lan>
2023-09-12 13:14:47 -0700

    Added dummy interest list scaffolding

---
+++ app/backend/profile/interest.py
@@ -0,0 +1,10 @@
+class Interest():
+    def __init__(self, interest: str, isApplicable: bool):
+        self.interest = interest
+        self.isApplicable = isApplicable
+
+    def to_json(self):
+        return {
+            "interest": self.interest,
+            "isApplicable": self.isApplicable
+        }
